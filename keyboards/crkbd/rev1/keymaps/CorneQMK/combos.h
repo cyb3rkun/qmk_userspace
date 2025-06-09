cfd6bc514768701e553bee845a7a63f7f78d3d26
cyb3rkun <cyb0rg@duck.com>
2025-06-09 23:51:14 +0200

    added easier backslash keymap on num_sym layer and added a combo

--- keyboards/crkbd/rev1/keymaps/CorneQMK/combos.h
+++ keyboards/crkbd/rev1/keymaps/CorneQMK/combos.h
@@ -1,3 +1,20 @@
+/*
+Copyright 2025 @cyb3rkun / @Cyb3r-Kun
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #pragma once
 
 // semi colon and colon combos
@@ -18,9 +35,9 @@ const uint16_t PROGMEM s_than[] = {KC_W, KC_E, COMBO_END};
 const uint16_t PROGMEM g_than[] = {KC_I, KC_O, COMBO_END};
 
 //grave
-const uint16_t PROGMEM grave[] = {KC_S, KC_D, COMBO_END};
+const uint16_t PROGMEM grave[] = {KC_A, KC_S, COMBO_END};
 //tilde
-const uint16_t PROGMEM tilde[] = {KC_A, KC_S, COMBO_END};
+const uint16_t PROGMEM tilde[] = {KC_S, KC_D, COMBO_END};
 //pipe
 const uint16_t PROGMEM pipe[] = {KC_X, KC_C, COMBO_END};
 //backslash
@@ -43,6 +60,12 @@ const uint16_t PROGMEM minus[] = {KC_V, KC_M, COMBO_END};
 const uint16_t PROGMEM underscore[] = {KC_B, KC_N, COMBO_END};
 // exclamation mark
 const uint16_t PROGMEM exclamation[] = {KC_C, KC_COMM, COMBO_END};
+// caret
+const uint16_t PROGMEM caret[] = {KC_T, KC_Y, COMBO_END};
+// percentage
+const uint16_t PROGMEM percent[] = {KC_DOT, KC_SLSH, COMBO_END};
+// hashtag/pound
+const uint16_t PROGMEM hash[] = {KC_Z, KC_X, COMBO_END};
 
 // Utilities
 // capslock
@@ -71,7 +94,6 @@ combo_t key_combos[] = {
     COMBO(tilde, KC_TILD),
     COMBO(pipe, KC_PIPE),
     COMBO(backslash, KC_BSLS),
-    // COMBO(g_than, KC_RABK),
 
     COMBO(asterisk, KC_ASTR),
     COMBO(plus, KC_PLUS),
@@ -81,6 +103,10 @@ combo_t key_combos[] = {
     COMBO(minus, KC_MINS),
     COMBO(underscore, KC_UNDS),
     COMBO(exclamation, KC_EXLM),
+    COMBO(caret, KC_CIRC),
+    COMBO(percent, KC_PERC),
+    COMBO(hash, KC_HASH),
+
 
     COMBO(capslock, KC_CAPS),
 
